ea4578d58ca2649f35d73343cbb330449b00140f
OHtmdwns <tmdwns1521@naver.com>
2024-02-21 18:12:32 +0900

    api 수정

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -3,13 +3,21 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 
+@Service
 public class MemberService {
     
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
--- src/test/java/com/example/demo/service/MemberServiceTest.java
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -4,63 +4,62 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
 import com.example.demo.domain.Member;
 
-public class MemberServiceTest {
+// public class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+//     MemberService memberService;
     
-    @Test
-    void 회원가입() {
-        // given
-        Member member = new Member();
-        member.setName("OSJ");
+//     @Test
+//     void 회원가입() {
+//         // given
+//         Member member = new Member();
+//         member.setName("OSJ");
 
-        // when
-        Long saveId = memberService.join(member);
+//         // when
+//         Long saveId = memberService.join(member);
 
-        // then
-        Member findMember = memberService.findOne(saveId).get();
-        Assertions.assertEquals(findMember.getId(), saveId); 
-    }
+//         // then
+//         Member findMember = memberService.findOne(saveId).get();
+//         Assertions.assertEquals(findMember.getId(), saveId); 
+//     }
 
-    @Test
-    void 중복_회원_예외() {
-        // given
-        Member member1 = new Member();
-        member1.setName("OSJ");
+//     @Test
+//     void 중복_회원_예외() {
+//         // given
+//         Member member1 = new Member();
+//         member1.setName("OSJ");
 
-        Member member2 = new Member();
-        member2.setName("OSJ");
+//         Member member2 = new Member();
+//         member2.setName("OSJ");
 
-        // when
-        Long saveId1 = memberService.join(member1);
+//         // when
+//         Long saveId1 = memberService.join(member1);
 
-        Long saveId2 = memberService.join(member2);
+//         Long saveId2 = memberService.join(member2);
 
-        // then
-        Member findMember1 = memberService.findOne(saveId1).get();
-        Member findMember2 = memberService.findOne(saveId2).get();
-        Assertions.assertEquals(findMember1.getId(), findMember2.getId()); 
-    }
+//         // then
+//         Member findMember1 = memberService.findOne(saveId1).get();
+//         Member findMember2 = memberService.findOne(saveId2).get();
+//         Assertions.assertEquals(findMember1.getId(), findMember2.getId()); 
+//     }
     
-    @Test
-    void findMembers() {
-        // given
+//     @Test
+//     void findMembers() {
+//         // given
 
-        // when
+//         // when
 
-        // then
-    }
+//         // then
+//     }
 
-    @Test
-    void findOne() {
-        // given
+//     @Test
+//     void findOne() {
+//         // given
 
-        // when
+//         // when
 
-        // then
-    }
-}
+//         // then
+//     }
+// }
